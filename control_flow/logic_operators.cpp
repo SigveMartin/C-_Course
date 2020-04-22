76832578a5ecfc1cb589e3781d0cf5dd838d71d6
SigveMartin <sigvemar@gmail.com>
2020-04-22 20:09:32 +0200

    update with control-flow chapter

---
+++ control_flow/logic_operators.cpp
@@ -0,0 +1,43 @@
+/*Goal: understand the logical operators supported by C++
+*/
+
+#include<iostream>
+#include<string>
+
+int main()
+{
+    int A = 5;
+    int B = 4;
+    int C = 5;
+    int D = 0;
+
+    std::string TorF[] = {"False", "True"};
+
+    std::cout << "A= " <<A<<"\n";
+    std::cout << "B= " <<B<<"\n";
+    std::cout << "C= " <<C<<"\n";
+    std::cout << "D= " <<D<<"\n";
+    //The && operator
+    std::cout<<"\nA == C is "<<TorF[A==C];
+    std::cout<<"\n(B == D) is "<<TorF[B==D];
+    std::cout<<"\n(B > D) is "<<TorF[B>D];
+    //A true && false = false
+    std::cout<<"\n\n(A ==C) && (B == D) is "<<TorF[(A ==C) && (B == D)];
+    //A true and true = true
+    std::cout<<"\n(A ==C) && (B > D) is "<<TorF[(A ==C) && (B > D)];
+
+    //The || operator
+    //A true || false = true
+    std::cout<<"\n\n(A ==C) || (B == D) is "<<TorF[(A ==C) || (B == D)];
+    //A true || true = true
+    std::cout<<"\n(A ==C) || (B > D) is "<<TorF[(A ==C) || (B > D)];
+
+    //The 'Not' operator
+    std::cout<<"\n\nA < B is "<<TorF[A<B];
+    std::cout<<"\n!(A < B) is "<<TorF[!(A<B)];
+
+    std::cout<<"\n\nA == C is "<<TorF[A==C];
+    std::cout<<"\n!(A == C) is "<<TorF[!(A==C)]<<"\n";
+
+    return 0;
+}
